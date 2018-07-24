c4a4ad808b33494cf94743af82313e16f102f90e
Sher Azam Hussain <azamhussain.25@iiee.edu.pk>
2018-07-24 12:36:03 -0600

    implement definition of functions in memory.c

--- Final_Assesment/src/memory.c
+++ Final_Assesment/src/memory.c
@@ -16,27 +16,32 @@
  * writing to memory via function calls. There is also a globally
  * allocated buffer array used for manipulation.
  *
- * @author Alex Fosdick
- * @date April 1 2017
+ * @author Azam
+ * @date Jul 20, 2018
  *
  */
 #include <memory.h>
-
+#include <stdint.h>
 /***********************************************************
- Function Definitions
+                Function Definitions
 ***********************************************************/
+
+/*****************set_value()***********************/
 void set_value(char * ptr, unsigned int index, char value){
   ptr[index] = value;
 }
 
+/*****************clear_value()***********************/
 void clear_value(char * ptr, unsigned int index){
   set_value(ptr, index, 0);
 }
 
+/*****************get_value()***********************/
 char get_value(char * ptr, unsigned int index){
   return ptr[index];
 }
 
+/*****************set_value()***********************/
 void set_all(char * ptr, char value, unsigned int size){
   unsigned int i;
   for(i = 0; i < size; i++) {
@@ -44,7 +49,89 @@ void set_all(char * ptr, char value, unsigned int size){
   }
 }
 
+/*****************clear_all()***********************/
 void clear_all(char * ptr, unsigned int size){
   set_all(ptr, 0, size);
+}
+
+/*****************my_memmove()***********************/
+uint8_t * my_memmove(uint8_t *src, uint8_t *dst, uint8_t length){
+
+
+}
+
+/*****************my_memcopy()***********************/
+uint8_t * my_memcopy(uint8_t *src, uint8_t *dst, uint8_t length){
+  int i ;
+  for (i = 0; i < length; i++)
+  {
+    *( dst + i ) = *( src + i );
+  }
+
+  return dst;
+
+}
+
+/*****************my_memset()***********************/
+uint8_t * my_memset(uint8_t *src, uint8_t length, uint8_t value){
+  unsigned char i ;
+
+  for ( i = 0; i < length; ++i)
+  {
+    *( src + i ) = value ;
+  }
+
+  return src ;
+
+}
+
+/*****************my_memzero()***********************/
+uint8_t * my_memzero(uint8_t *src, size_t length){
+	uint8_t i;
+
+  for (i = 0; i < length; ++i)
+  {
+    *( src + i ) = '0' ;
+  }
+
+  return src;
+
+}
+
+/*****************my_reverse()***********************/
+uint8_t * my_reverse(uit8_t *src, size_t length){
+  int lo;
+  int hi;
+
+  for( lo = 0, hi = length - 1; hi > lo; lo++, hi--)
+    {
+        
+      uint8_t *tmp = src[lo];
+      src[lo] = src[hi];
+      src[hi] = tmp;
+
+    }
+
+    return src;
+
+
+}
+
+/*****************reserve_words()***********************/
+uint32_t * reserve_words(size_t length){
+  uint32_t *word_ptr;
+  word_ptr = calloc( length, sizeof(uint32_t)); 
+  if( word_ptr == NULL )
+    return NULL;
+  else
+    return word_ptr;
+
+}
+
+/*****************free_words()***********************/
+void free_words(uint32_t *src){
+  if( src != NULL )
+    free(src);
+
 }
 
